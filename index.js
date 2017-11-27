6e7a449fc9aa9da98eebb754f2533dd6d7dc8b68
Berndinox <berndinox@gmail.com>
2017-11-27 08:16:25 +0000

    update

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ app.get('/', function (req, res) {
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(80, function(){
