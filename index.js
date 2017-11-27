ee53ccbc6dd48cc217067a70ca9997cbb0cbab0b
Berndinox <berndinox@gmail.com>
2017-11-27 12:20:58 +0000

    Update

--- index.js
+++ index.js
@@ -8,19 +8,36 @@ var iotajs = new IOTA({
     'provider': 'https://iota.offline.zone:443'
 });
 
-app.use(express.static('public'));
 app.set('view engine', 'ejs')
-
 
 app.get('/', function (req, res) {
     res.render('index');
 })
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+var nodeInfo;
+function nodeInfo() {
+    iotajs.api.getNodeInfo(function(error, success) {
+    if (error) {
+        console.error(error);
+    } else {
+        nodeInfo = success;
+    }
+})
+}
+setInterval(nodeInfo, 1000); //time is in ms
+
+
+
+
+io.sockets.on('connection', function (socket) {
+    setInterval(function(){
+      io.sockets.emit('nodeInfo', nodeInfo);
+    }, 1000);
+    //socket.on('disconnect', function () {
+        //user disconnected
+        //emit to every opened socket, so everyone has up to date data
+    //    io.sockets.emit('nodeInfo', nodeInfo);
+    //});
 });
 
 http.listen(80, function(){
